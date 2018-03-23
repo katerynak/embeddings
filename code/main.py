3fc1fa08a3d32b9a7ae26f32fd0bd1cbd2700c3f
katerynak <konotopska.k@gmail.com>
2018-03-23 13:55:05 +0100

    added labels

--- code/main.py
+++ code/main.py
@@ -8,14 +8,16 @@ import utils
 
 if __name__=="__main__":
     s = load.load()
-    sampling_n = np.geomspace(12, utils.max_len(s), num=5)
+    sampling_n = np.geomspace(12, utils.max_len(s), num=10)
     sampling_n = [int(i) for i in sampling_n]
     #sampling_n = [12,24]
-    stress_samples = 1000
+    stress_samples = 100
     stress = []
     for n in sampling_n:
         stress.append(utils.eval_stress(s,nb_points=n,num_rows_2_sample=stress_samples))
     print (sampling_n)
     print(stress)
+    plt.xlabel('n. resampling points')
+    plt.ylabel('stress')
     plt.plot(sampling_n, stress)
     plt.show()
