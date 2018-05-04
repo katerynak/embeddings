2d2a8b3b5313e990bf4fa6c64a136a10d98f17d8
katerynak <konotopska.k@gmail.com>
2018-05-04 18:00:22 +0200

    possible configs for lmds evaluation

--- code/main.py
+++ code/main.py
@@ -11,12 +11,15 @@ import lipschitz
 import distances as dist
 import lmds
 import random
+import os
+import time
 
 
 if __name__=="__main__":
     s = load.load()
     s_idx = list(range(0, len(s)))
-    random.seed(0)
+    data_seed = 0
+    random.seed(data_seed)
     random.shuffle(s_idx)
     s_size = 50000
     s = s[s_idx][:s_size]
@@ -132,28 +135,57 @@ if __name__=="__main__":
         plt.show()
 
     if 'LMDS_EMBEDDING' in globals():
-        #results_dir = '../eval_results/lmds'
-        sterss_samples = 100
-        k = [2,4,8,10,12]
-        n_landmarks = [30,30,30,30,30]
+        results_dir = "../eval_results/lmds"
+        if not os.path.exists(results_dir):
+            os.makedirs(results_dir)
+        stress_samples = 100
+        k = [2,4,8,10]
+        n_landmarks = [10,20,25,30,40]
+        #stress_seed = 4
         stress = []
         inv_corr = []
         distortion = []
-        for (n,l) in zip(k, n_landmarks):
-            embeddings = lmds.compute_lmds(s, nl=l, k=n, distance=dist.original_distance)
-            original_distances, embedded_distances = utils.eval_distances(s, embeddings,
-                                                                          num_rows_2_sample=sterss_samples)
-            if 'STRESS' in globals():
-                stress.append(em.stress(dist_original=original_distances,
-                                        dist_embedd=embedded_distances))
-            # print("dissimilarity stress: ", stress)
-            if 'PEARSON_CORRELATION' in globals():
-                inv_corr.append(
-                    em.inverse_correlation(dist_original=np.ndarray.flatten(np.asarray(original_distances)),
-                                           dist_embedd=np.ndarray.flatten(np.asarray(embedded_distances))))
-            if 'DISTORTION' in globals():
-                distortion.append(em.distorsion(dist_original=original_distances,
-                                                dist_embedd=embedded_distances))
+        for data_seed in range(4):
+            s_idx = list(range(0, len(s)))
+            data_seed = 0
+            random.seed(data_seed)
+            random.shuffle(s_idx)
+            s_size = 50000
+            s = s[s_idx][:s_size]
+            for n in k:
+                for l in n_landmarks:
+                    for stress_seed in range(4):
+                        start_time = time.time()
+                        embeddings = lmds.compute_lmds(s, nl=l, k=n, distance=dist.original_distance)
+                        comp_time = time.time()-start_time
+
+                        original_distances, embedded_distances = utils.eval_distances(s, embeddings,
+                                                                                      num_rows_2_sample=stress_samples,
+                                                                                      seed=stress_seed)
+                        if 'STRESS' in globals():
+                            stress.append(em.stress(dist_original=original_distances,
+                                                    dist_embedd=embedded_distances))
+                        # print("dissimilarity stress: ", stress)
+                        if 'PEARSON_CORRELATION' in globals():
+                            inv_corr.append(
+                                em.inverse_correlation(dist_original=np.ndarray.flatten(np.asarray(original_distances)),
+                                                       dist_embedd=np.ndarray.flatten(np.asarray(embedded_distances))))
+                        if 'DISTORTION' in globals():
+                            distortion.append(em.distorsion(dist_original=original_distances,
+                                                            dist_embedd=embedded_distances))
+                        resFileName = results_dir + "/" + "k_" + str(n) + \
+                                      "__n_landmarks_" + str(l) + \
+                                      "__data_seed_" + str(data_seed) + \
+                                      "__eval_seed_" + str(stress_seed)
+                        with open(resFileName,'w') as f:
+                            f.write('stress\t' + str(stress[-1]) + '\n')
+                            f.write('inverse_correlation\t' + str(inv_corr[-1]) + '\n')
+                            f.write('distortion\t' + str(distortion[-1]) + '\n')
+                            f.write('n_streamlines\t' + str(len(embeddings))+ '\n')
+                            f.write('exec_time\t' + str(comp_time) + '\n')
+                            f.write('eval_samples\t' + str(stress_samples) + '\n')
+                            f.write('k\t'+ str(n)+'\n')
+                            f.write('n_landmarks\t' + str(l)+'\n')
         print("Stress: ", stress)
         print("Correlation distance: ", inv_corr)
         print("Distiortion: ", distortion)
