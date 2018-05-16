f0c0f9f76c177747862863897af94974e837c235
Emanuele Olivetti <emanuele.olivetti@gmail.com>
2018-05-16 22:59:10 +0200

    added test on streamlines (work in progress) + minor improvements

--- code/test_embeddings.py
+++ code/test_embeddings.py
@@ -27,33 +27,42 @@ def sph2cart(az, el, r):
 
 
 if __name__ == '__main__':
-    np.random.seed(1)
+    np.random.seed(0)
     N = 10000
     d = 20
     X = np.random.uniform(size=(N, d))
     k = 14
+    distance = euclidean_distance
 
     # alpha = np.random.uniform(low=0.0, high=(2.0 * np.pi), size=N)
     # beta = np.random.uniform(low=0.0, high=(2.0 * np.pi), size=N)
     # X = np.array([sph2cart(alpha[i], beta[i], 1.0) for i in range(N)])
     # k = 2
 
+    # from load import load
+    # from dipy.tracking.distances import bundles_distances_mam
+    # X = np.array(load(), dtype=np.object)
+    # idx = np.random.permutation(X.shape[0])[:10000]
+    # X = X[idx]
+    # distance = bundles_distances_mam
+    # k = 20
+
     print("Estimating the quality of embedded distances vs. original distances.")
 
     print("Fastmap:")
     subsample = False
     n_clusters = 10
-    Y = fastmap(X, euclidean_distance, k, subsample, n_clusters)
+    Y = fastmap(X, distance, k, subsample, n_clusters, verbose=True)
 
     idx = np.random.permutation(len(X))[:1000]
-    D_sub = distance_matrix(X[idx], X[idx])
+    D_sub = distance(X[idx], X[idx])
     DY_sub = distance_matrix(Y[idx], Y[idx])
     print("  Correlation: %s" % (np.corrcoef(D_sub.flatten(), DY_sub.flatten())[0, 1]))
     print("  Stress : %s" % (stress(D_sub.flatten(), DY_sub.flatten())))
 
     print("lMDS:")
     lmds_embedding = np.array(compute_lmds2(X, nl=100, k=k,
-                                             distance=euclidean_distance,
+                                             distance=distance,
                                              landmark_policy='sff'))
     D_lmds_sub = distance_matrix(lmds_embedding[idx], lmds_embedding[idx])
     print("  Correlation: %s" % (np.corrcoef(D_sub.flatten(), D_lmds_sub.flatten())[0, 1]))
@@ -61,7 +70,7 @@ if __name__ == '__main__':
 
     print("Dissimilarity Representation:")
     dissimilarity_embedding, prototype_idx = compute_dissimilarity(X, num_prototypes=40,
-                                                                   distance=euclidean_distance,
+                                                                   distance=distance,
                                                                    verbose=False)
     D_dissimilarity_sub = distance_matrix(dissimilarity_embedding[idx], dissimilarity_embedding[idx])
     print("  Correlation: %s" % (np.corrcoef(D_sub.flatten(), D_dissimilarity_sub.flatten())[0, 1]))
