5489c83c6c9231a80c0392574d61d54b484f2cc3
katerynak <konotopska.k@gmail.com>
2018-05-30 11:48:03 +0200

    code cleaning

--- code/test_embeddings_streamlines.py
+++
@@ -1,9 +0,0 @@
-from lmds import compute_lmds
-import load
-import distances as dist
-
-
-if __name__ == '__main__':
-    tracks = load.load()
-    lmds_embeddings = compute_lmds(tracks, nl=20, k=4,
-                                   distance=dist.original_distance)
