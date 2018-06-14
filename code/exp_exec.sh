a7d21a8a942e6f6ede4e9bc3b14165bcd4b57f86
katerynak <konotopska.k@gmail.com>
2018-06-14 10:19:31 +0200

    small bug fixed

--- code/exp_exec.sh
+++ code/exp_exec.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 #declare -a tracks=("100307" "102311" "109123")
-declare -a embeddings=("lmds", "fastmap", "dissimilarity", "lipschitz", "resampling")
+declare -a embeddings=("lmds" "fastmap" "dissimilarity" "lipschitz" "resampling")
 declare -a tracks=( "109123")
 for t in "${tracks[@]}"
     do
